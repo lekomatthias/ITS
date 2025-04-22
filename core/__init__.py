48e0eb3d6e3a1fcabbe977b0a364b853b09b132a
lekomatthias <lekomatthias@gmail.com>
2025-04-22 15:14:26 -0300

    mudança main

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,9 @@
-
-from .knn_slic_metric_clustering_plus import ClusteringClassifier2, main
+from .knn_slic import PixelClassifier2
+from .knn_slic_metric import SuperpixelClassifier2
+from .knn_slic_metric_clustering_plus import ClusteringClassifier2
 
 __all__ = [
+    'PixelClassifier2',
+    'SuperpixelClassifier2',
     'ClusteringClassifier2',
-    'main',
 ]
--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+
+from tkinter import filedialog
 from core import *
 
-main()
+if __name__ == "__main__":
+
+    classifier = ClusteringClassifier2(num_segments=200)
+    # classifier.SP_divide()
+    # classifier.Train()
+    # classifier.classify(threshold=5.8, show_data=False)
+    path = filedialog.askopenfilename(title="Selecione a imagem para aplicação",
+                                                            filetypes=[("Imagens", "*.jpeg;*.jpg;*.png")])
+    alg_list = [
+    "DBSCAN", 
+     "KMeans", 
+     "AgglomerativeClustering", 
+     "OPTICS", 
+     "AffinityPropagation", 
+     "MeanShift", 
+     "GaussianMixture", 
+     "Birch", 
+     "FuzzyCMeans", 
+     "SOM"
+     ]
+    for alg in alg_list: 
+        classifier.Type_visualization(image_path=path, method=alg, show_inertia=False)
